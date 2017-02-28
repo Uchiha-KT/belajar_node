b03b693abf2467925c37fef7fa4cf183cfa3d30a
Uchiha-KT <r.agusnaidi@gmail.com>
2017-02-28 21:22:34 +0700

    Laerning about Routing

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-var http = require("http");
+var http   = require("http");
+var url    = require("url");
+var routes = require("routes")();
+
+routes.addRoute('/', function(req, res){
+  res.writeHead(200,{"Content-Type" : "text/plain"});
+  res.end("This is index page");
+});
+
+routes.addRoute('/profile/:nama?', function(req, res){
+  res.writeHead(200,{"Content-Type" : "text/plain"});
+  if (this.params.nama != "undefined") {
+
+  res.end("Profile Pagenya: "+this.params.nama);
+  }
+});
 
 http.createServer(function(req, res){
-  res.writeHead(200, {"Content-Type" : "text/plain"});
-  res.write("Hello form JS Server");
-  res.end();
+  var path = url.parse(req.url).pathname;
+  var match = routes.match(path);
+  if (match) {
+    match.fn(req, res);
+  }
+  else{
+    res.writeHead(200,{"Content-Type" : "text/plain"});
+    res.end("Error 404");
+  }
 }).listen(8080);
